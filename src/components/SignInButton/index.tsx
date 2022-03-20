e55f3ebacc4f3cbdcfd153cbd612ab00e406781e
Elton Mozart <elton.m.da.silva@accenture.com>
2022-03-19 21:55:50 -0300

    Configurações iniciais e login de usuário

---
+++ src/components/SignInButton/index.tsx
@@ -0,0 +1,22 @@
+import { FaGithub } from 'react-icons/fa';
+import { FiX } from 'react-icons/fi';
+import { signIn, useSession } from 'next-auth/client';
+
+import styles from './styles.module.scss'
+
+export function SignInButton() {
+  const [ session ] = useSession();
+  
+  return session ? (
+    <button type="button" className={styles.signInButton}>
+      <FaGithub color='#04D361'/>
+      Elton Mozart
+      <FiX color='#737380' className={styles.closeIcon}/>
+    </button>
+  ) : (
+    <button type="button" className={styles.signInButton} onClick={() => signIn('github')}>
+      <FaGithub color='#EBA417'/>
+      Sign In with Github
+    </button>
+  )
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,60 @@
+import { GetStaticProps } from 'next'
+import Head from 'next/head'
+
+import styles from './home.module.scss'
+
+import type { NextPage } from 'next'
+import { SubscribeButton } from '../components/SubscribeButton'
+import { stripe } from '../services/stripe'
+
+interface HomeProps {
+  product: {
+    priceId: string,
+    amount: number
+  }
+}
+
+const Home: NextPage = ({ product }: HomeProps) => {
+  return (
+    <>
+      <Head>
+        <title>ig.news | Home</title>
+      </Head>
+      <main className={styles.contentConteiner}>      
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>News about the <span>React</span> world.</h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for {product.amount} month</span>
+          </p>
+          <SubscribeButton priceId={product.priceId}/>
+        </section>
+        <img src='/images/avatar.svg' alt='Girl coding' />
+      </main>
+    </>
+    
+  )
+}
+
+export default Home;
+
+export const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve('price_1KV2CaKjDhkpIGn9LPKbuilM', {
+    expand: ['product']
+  })
+
+  const product = {
+    priceID: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD'
+    }).format(price.unit_amount / 100)
+  }
+  return {
+    props: {
+      product
+    },
+    revalidate: 60 * 60 * 24 // 24hs
+  }
+}
