66b20ee8e677a9757f687086681b02ca0fa9ace2
Elton Mozart <elton.m.da.silva@accenture.com>
2022-03-19 23:12:46 -0300

    Atualização dos nomes dos pacotes de acordo com a documentação mais recente do nextauth

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,11 +1,11 @@
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
-import { signIn, useSession } from 'next-auth/client';
+import { signIn, useSession } from 'next-auth/react';
 
 import styles from './styles.module.scss'
 
 export function SignInButton() {
-  const [ session ] = useSession();
+  const { data: session } = useSession();
   
   return session ? (
     <button type="button" className={styles.signInButton}>
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,15 @@
 import type { AppProps } from 'next/app'
 import { Header } from '../components/Header'
-import { Provider as NextAuthProvider} from 'next-auth/client'
+import { SessionProvider as NextAuthSessionProvider} from 'next-auth/react'
 
 import '../styles/global.scss'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <NextAuthProvider session={pageProps.session}>
+    <NextAuthSessionProvider session={pageProps.session}>
       <Header />
       <Component {...pageProps} />
-    </NextAuthProvider>
+    </NextAuthSessionProvider>
   )
 }
 
